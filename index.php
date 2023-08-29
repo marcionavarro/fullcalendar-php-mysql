4b0fac2a7d396cb57c65328f707216edffee1533
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-08-29 20:18:34 -0300

    tradução, adicionando opções e modal

--- index.php
+++ index.php
@@ -14,21 +14,65 @@
 </head>
 <body>
 
-<div id='calendar'></div>
+<div class="container">
+    <div class="col-md-12">
+        <div id='calendar'></div>
+    </div>
 
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#modalEvent">
+        Launch
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="modalEvent" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="modelTitleId">Modal Title</h4>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Body
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"></script>
 
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
+    var modalEvent
+    modalEvent = new bootstrap.Modal(document.getElementById('modalEvent'), {keyboard: false})
+
+    document.addEventListener('DOMContentLoaded', function () {
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
-            initialView: 'dayGridMonth'
+            //initialView: 'dayGridMonth',
+            locale: "pt",
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            },
+            dateClick: function (information) {
+                alert("Pressionado " + information.dateStr);
+                modalEvent.show();
+            }
         });
         calendar.render();
     });
 </script>
-
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"></script>
 
 </body>
 </html>
